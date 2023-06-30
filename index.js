d8bbec040559858a5e33b9530b3a3f2ecd288ae7
godiarksel <godiarksel@gmail.com>
2023-06-30 09:48:37 +0300

    Do code refactor

--- index.js
+++ index.js
@@ -8,62 +8,9 @@ context.fillRect(0,0, canvas.width, canvas.height);
 
 const gravity = 0.5;
 
-class Sprite {
-    constructor({position, velocity, colour = 'red', offset}){
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 50;
-        this.height = 150;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        };
-        this.colour = colour;
-        this.isAttacking
-        this.health = 100;
-    }
 
-    draw(){
-        context.fillStyle = this.colour;
-        context.fillRect(this.position.x, this.position.y, this.width, this.height);
 
-         if(this.isAttacking){       
-            context.fillStyle = 'yellow';
-            context.fillRect(this.attackBox.position.x, 
-                            this.attackBox.position.y, 
-                            this.attackBox.width, 
-                            this.attackBox.height)}
-        }
-
-    update(){
-        this.draw();
-
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-                this.velocity.y = 0;
-        } else this.velocity.y += gravity;
-    }
-
-    attack(){
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100)
-    }
-}
-
-const playerOne = new Sprite({
+const playerOne = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -80,7 +27,7 @@ const playerOne = new Sprite({
     },
 });
 
-const playerTwo = new Sprite({
+const playerTwo = new Fighter({
     position: {
         x: 400,
         y: 100
